f04c8193b8149efa2c4fe2eea1f036a3f6d5e011
João Guilherme J. Marinho <gjmarinho.joao@gmail.com>
2020-03-04 00:28:09 -0300

    Toques finais na estilizaçao, finalizaçao das animaçoes, inclusao do Footer e melhor colocaçao dos objetos na pagina

--- animations.js
+++ animations.js
@@ -10,27 +10,43 @@ function joaoMode() {
 }
 
 function hacked(){
-    alert("KKKKK JA ERA MANO, ATAK DO PATO LOKO")
+    alert("ATAK DO PATO LOKO")
 }
 
 function naniCrazyDuck(){
+
+    document.body.style.backgroundImage =  "url(Art/matrixg.gif)";    
     document.getElementsByClassName("crazyduck")[0].style.opacity = 1;
-    document.getElementsByTagName("h1")[0].textContent = "ATAK DO PATO HACKER";
-    document.body.style.backgroundImage =  "url(Art/matrixg.gif)";
-    document.getElementById("duckWithGun").style.backgroundImage = "url(Art/patoArmado.png)";
-    document.getElementById("duckWithGun").style.backgroundSize = "100% 100%";
-    document.getElementById("duckWithGun").textContent = ""
+    document.getElementsByTagName("h1")[0].textContent = "ATAK DO PATO LOKO";
+
+    let style = document.createElement('style');
+    document.head.appendChild(style);
+    style.sheet.insertRule('.color-change {color: aliceblue}');
+
+
+    feelings = ["PARANOIA", "VIOLÊNCIA", "DESHONRA"];
+    images = ["url(Art/lanseabraba.jpg", "url(Art/patoArmado.png", "url(Art/duckwithknife.jpg"];    
+    bullets = document.getElementsByClassName("bullet-content");
+    bulletTitles = document.getElementsByClassName("bullet-title");
     buttons = document.getElementsByTagName("button");
-    for (let i = 0; i < buttons.length; i++) {
+
+    for (let i = 0; i < buttons.length && i < bullets.length ; i++) {
         buttons[i].setAttribute( "onClick", "javascript: hacked();" );
+        bullets[i].textContent = "";
+        bullets[i].style.backgroundImage = images[i];
+        bullets[i].style.backgroundSize = "100% 100%";
+        bulletTitles[i].textContent = feelings[i];
     }
+    
+
+
     document.getElementsByClassName("jumbobottomright")[0].innerHTML = `🦆ÅŤÅQŮĘ ĐØ P4TØ ĽØĶØ🦆<br><br>
     🦆SAIA DO SITE🦆    🦆COMEÇOU O ATAQUE🦆 ❗❗<br>
     HÁ! HÁ! HÁ! HÁ! HÁ❗❗❗❗<br>
     🦆 ❗ 🦆 ❗ 🦆 ❗<br><br>
-    ̿̿ ̿'̿'\̵͇̿̿\з= 🦆 =ε/̵͇̿̿/’̿’̿ ̿ ̿̿<br>
+    ̿̿ ̿'̿'\\\̵͇̿̿\\з= 🦆 =ε/̵͇̿̿/’̿’̿ ̿ ̿̿<br>
             (   .  )  <br>
-          __//    \\__  <br>
+          __//   \\\\__  <br>
     Ta-_Em_-CHoK kk❓ ⚡⚡⚡⚡  🦆 ❗  <br><br>
     VØÇË§ FØŘÅM ÅŤÅČÅĐØ§ PËĽØ P4TØ ĽØĶØ<br><br>
     ▕▇▇▇◤▔▔▔▔▔▔▔◥▇▇▇  <br>
@@ -47,5 +63,4 @@ function naniCrazyDuck(){
     scroll(0,0);
         
 }
-
 
